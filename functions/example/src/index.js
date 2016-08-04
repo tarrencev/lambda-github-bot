02d7d35156e00375df4987917e96261782d061bd
Tarrence van As <tarrence@box.com>
2016-08-04 14:45:00 -0700

    Cleanup

--- functions/example/src/index.js
+++ functions/example/src/index.js
@@ -3,7 +3,7 @@ import λ from 'apex.js';
 import github from 'octonode';
 
 export default λ(event => {
-    const message = event.Records[0].Sns.Message;
+    const message = JSON.parse(event.Records[0].Sns.Message);
     const { repository } = message;
     const { owner } = repository;
     const { name } = owner;
@@ -12,12 +12,10 @@ export default λ(event => {
         return Promise.reject('Invalid repository');
     }
 
-    console.log('From SNS:', message);
     const ghClient = github.client(process.env.GITHUB_TOKEN);
     return new Promise((resolve) => {
         ghClient.get('/user', {}, function (err, status, body, headers) {
-            console.log(body);
-            resolve(body); //json object
+            resolve(body);
         });
     });
 });
